04bdfddc5f7a4566ae0d39ec655a0d0d3ef68a99
Bente Christine Aasgaard <bca@simula.org>
2016-06-06 15:09:32 +0200

    Removed bootstrap.sh and created separate bootstrap-files for centos and ubuntu.

--- vagrant/bootstrap-centos.sh
+++ vagrant/bootstrap-centos.sh
@@ -3,5 +3,6 @@
 rpm -ivh http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
 yum -y groupinstall "PostgreSQL Database Server 9.3 PGDG"
 yum -y install python-setuptools gcc postgresql93-devel python-devel
-
-
+yum -y install nmap
+yum -y install emacs
+chkconfig postgresql-9.3 on
