782f04a69b8f58cabfea72ac5e2077e32b7163d1
Bente Christine Aasgaard <bca@simula.org>
2016-06-06 15:09:32 +0200

    Removed bootstrap.sh and created separate bootstrap-files for centos and ubuntu.

---
+++ vagrant/bootstrap-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+apt-get -y install postgresql-9.3
+apt-get -y install postgresql-server-dev-9.3
+apt-get -y install libpq-dev
+apt-get -y install nmap
+apt-get -y install emacs
+apt-get -y install gcc 
+apt-get -y install python-setuptools
+#apt-get install postgresql93-devel
+apt-get -y install python-dev
+# chkconfig postgresql-9.3 on
